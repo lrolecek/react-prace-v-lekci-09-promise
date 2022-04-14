b22014df96e8e86c034c69bbb869deaf5db74256
Luděk Roleček <ludek@rolecek.cz>
2022-04-14 18:51:47 +0200

    Aktualizace

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // opakování fetch a promise
 fetch('https://swapi.dev/api/people/1')
-.then((response) => {
-	console.log('data jsou tady');
-	console.log(response);
-});
+.then(response => response.json())
+.then(data => {
+	console.log(data);
+})
